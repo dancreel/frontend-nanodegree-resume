3a4d94d0b8bd6b75ee67f5d58828c128782bc003
Dan Creel <dancreel@google.com>
2015-02-01 21:12:03 -0500

    Clean up resumeBuilder.js to only include bio, education, work and projects objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,6 @@
-var name = "Daniel G. Creel";
-var role = "Attorney; Development Professional";
-var skills = ["business law", "non-profits", "trademark & copyright", "veterans benefits", "cultural history", "media", "education", "development", "PHP", "MySQL", "HTML", "CSS", "JS", "Git/GitHub"];
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
 var bio = {
-	"name": name,
-	"role": role,
+	"name": "Daniel G. Creel",
+	"role": "Attorney; Development Professional",
 	"contacts": {
 		"email": "dancreel@gmail.com",
 		"mobile": "301-814-6668",
@@ -19,75 +9,12 @@ var bio = {
 		"blog": "",
 		"location": "Gaithersburg, MD",
 	},
-	"picture": "./images/me.jpg",
-	"welcome": "Hello world!",
-	"skills": skills
+	"bioPic": "/images/me.jpg",
+	"welcomeMessage": "Hello world!",
+	"skills": [
+		"business law", "non-profits", "trademark & copyright", "veterans benefits", "cultural history", "media", "education", "development", "PHP", "MySQL", "HTML", "CSS", "JS", "Git/GitHub"
+	];
 };
-
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
-var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcome);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-
-$("#topContacts")
-	.append(formattedEmail)
-	.append(formattedMobile)
-	.append(formattedTwitter)
-	.append(formattedGitHub)
-	.append(formattedBlog)
-	.append(formattedLocation);
-
-$("#header")
-	.prepend(formattedPicture)
-	.append(formattedWelcome)
-	.append(HTMLskillsStart)
-	.append(formattedSkills);
-
-var work = {
-	"employer": "The Catholic University of America",
-	"title": "Director of Advancement Services",
-	"dates": "2014-present",
-	"location": "Washington, D.C.",
-	"description": "Directs and integrates all functions in the Department of Advancement Services that are essential to the efficient and effective execution of the university’s fundraising, volunteer engagement, and campaign management efforts. Serves as a visible leader-manager to evaluate operations, analyze data and information, and recommend solutions to help achieve strategic division and institutional goals. Closely collaborates with colleagues to ensure that University Advancement resources are used effectively to grow a culture of increased philanthropy and volunteer leadership to the university. Supervises the Advancement Services Department of the Division of University Advancement."
-};
-
-var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
-var formattedTitle = HTMLworkTitle.replace("%data%", work.title);
-var formattedDates = HTMLworkDates.replace("%data%", work.dates);
-var formattedLocation = HTMLworkLocation.replace("%data%", work.location);
-var formattedDescription = HTMLworkDescription.replace("%data%", work.description);
-
-$("#workExperience")
-	.append(HTMLworkStart)
-	.append(formattedEmployer)
-	.append(formattedTitle)
-	.append(formattedDates)
-	.append(formattedLocation)
-	.append(formattedDescription);
-
-var project = {
-	"title": "Project 1",
-	"dates": "2014",
-	"description": "description",
-	"image": ""
-};
-
-var formattedprojectTitle = HTMLprojectTitle.replace("%data%", project.title);
-var formattedprojectDates = HTMLprojectDates.replace("%data%", project.dates);
-var formattedprojectDescription = HTMLprojectDescription.replace("%data%", project.description);
-var formattedprojectImage = HTMLprojectImage.replace("%data%", project.image);
-
-$("#projects")
-	.append(HTMLprojectStart)
-	.append(formattedprojectTitle)
-	.append(formattedprojectDates)
-	.append(formattedprojectDescription)
-	.append(formattedprojectImage);
 
 var education = {
 	"schools": [
@@ -112,37 +39,145 @@ var education = {
 			"major": "Law",
 			"date": "2008"
 		}
+	],
+	"onlineCourses": [
+		{
+			"title": "How to Use Git and GitHub",
+			"school": "Udacity",
+			"date": "January 2015",
+			"URL": "https://www.udacity.com/course/ud775"
+		}
 	]
 };
 
-var formattedName = HTMLschoolName.replace("%data%", education.schools[0].name);
-var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[0].degree);
-var formattedDate = HTMLschoolDates.replace("%data%", education.schools[0].date);
-var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[0].location);
-var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[0].major);
-
-$("#education")
-	.append(HTMLschoolStart)
-	.append(formattedName)
-	.append(formattedDegree)
-	.append(formattedDate)
-	.append(formattedLocation)
-	.append(formattedMajor);
-
-var online = {
-	"title": "How to Use Git and GitHub",
-	"school": "Udacity",
-	"date": "January 2015",
-	"URL": "https://www.udacity.com/course/ud775"
+var work = {
+	"jobs": [
+		{
+			"employer": "The Catholic University of America",
+			"title": "Director of Advancement Services",
+			"dates": "2014-present",
+			"location": "Washington, D.C.",
+			"description": "Directs and integrates all functions in the Department of Advancement Services that are essential to the efficient and effective execution of the university’s fundraising, volunteer engagement, and campaign management efforts. Serves as a visible leader-manager to evaluate operations, analyze data and information, and recommend solutions to help achieve strategic division and institutional goals. Closely collaborates with colleagues to ensure that University Advancement resources are used effectively to grow a culture of increased philanthropy and volunteer leadership to the university. Supervises the Advancement Services Department of the Division of University Advancement."
+		},
+		{
+			"employer": "Future Soccer Club",
+			"title": "Director/Treasurer/Operations Administrator",
+			"dates": "2010-2013",
+			"location": "Silver Spring, Md.",
+			"description": "Responsible for general accounting such as monthly bank reconciliations, routine journal entries, general ledger account reconciliation, setting up of new GL accounts. Key contact for the bank and other external financial relationships. Responsible for all aspects of processing payroll for the organization using QuickBooks payroll services. Prepared all required federal and state tax and organizational filings (including annual 990 & 944, and initial 1023 application for non-profit status)."
+		},
+		{
+			"employer": "The Catholic University of America",
+			"title": "Director of Research and Prospect Management",
+			"dates": "2002-2014",
+			"location": "Washington, D.C.",
+			"description": "Served as Interim Director of Development Services from January 2012 to May 2012. Managed the Research Department of the Office of Institutional Advancement (OIA). Administered the prospect management functions of OIA including the building and management of prospect pools, compiling moves management reports and controlling the general flow of prospects. Research responds to requests for information on prospective donors from within OIA for use by development officers in advancing solicitations to secure support for CUA. Research is also responsible for proactively identifying potential donors to the university from a variety of sources including media and in-person screening sessions. Research is responsible for ensuring that updated information on alumni/donors is collected, documented, and shared with the OIA staff and external offices, such as the Deans’ Offices and the President’s Office."
+		},
+		{
+			"employer": "The Catholic University of America",
+			"title": "Director of Research and Prospect Management",
+			"dates": "2002-2014",
+			"location": "Washington, D.C.",
+			"description": "Served as Interim Director of Development Services from January 2012 to May 2012. Managed the Research Department of the Office of Institutional Advancement (OIA). Administered the prospect management functions of OIA including the building and management of prospect pools, compiling moves management reports and controlling the general flow of prospects. Research responds to requests for information on prospective donors from within OIA for use by development officers in advancing solicitations to secure support for CUA. Research is also responsible for proactively identifying potential donors to the university from a variety of sources including media and in-person screening sessions. Research is responsible for ensuring that updated information on alumni/donors is collected, documented, and shared with the OIA staff and external offices, such as the Deans’ Offices and the President’s Office."
+		},
+		{
+			"employer": "Discovery Channel",
+			"title": "DVD/Special Projects Manager",
+			"dates": "1999-2001",
+			"location": "Bethesda, Md.",
+			"description": "Headed all aspects of DVD production and development for Discovery Communications. Responsible for all day-to-day operations of the International Sales and Marketing group. Managed process for tracking rights and footage exceptions to ensure legal compliance before selling into the market. Responsible for working with regional sales directors to manage their expenses across business units. Worked with the Regional Network Operations groups to determine available programming by region. Supervised a fulfillment coordinator."
+		}
+	]
 };
 
-var formattedonlineTitle = HTMLonlineTitle.replace("%data%", online.title);
-var formattedonlineSchool = HTMLonlineSchool.replace("%data%", online.school);
-var formattedonlineDate = HTMLonlineDates.replace("%data%", online.date);
-var formattedonlineURL = HTMLonlineURL.replace("%data%", online.URL);
+var project = {
+	"projects": [
+		{
+			"title": "Project 1",
+			"dates": "2014",
+			"description": "description",
+			"image": ""
+		}
+	]
+};
 
-$("#education")
-	.append(HTMLonlineClasses)
-	.append(formattedonlineTitle)
-	.append(formattedonlineDate)
-	.append(formattedonlineURL);
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+//
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//
+//var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+//var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+//var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+//var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+//var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+//var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+//var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
+//var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcome);
+//var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+//
+//$("#topContacts")
+//	.append(formattedEmail)
+//	.append(formattedMobile)
+//	.append(formattedTwitter)
+//	.append(formattedGitHub)
+//	.append(formattedBlog)
+//	.append(formattedLocation);
+//
+//$("#header")
+//	.prepend(formattedPicture)
+//	.append(formattedWelcome)
+//	.append(HTMLskillsStart)
+//	.append(formattedSkills);
+//
+//var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+//var formattedTitle = HTMLworkTitle.replace("%data%", work.title);
+//var formattedDates = HTMLworkDates.replace("%data%", work.dates);
+//var formattedLocation = HTMLworkLocation.replace("%data%", work.location);
+//var formattedDescription = HTMLworkDescription.replace("%data%", work.description);
+//
+//$("#workExperience")
+//	.append(HTMLworkStart)
+//	.append(formattedEmployer)
+//	.append(formattedTitle)
+//	.append(formattedDates)
+//	.append(formattedLocation)
+//	.append(formattedDescription);
+//
+//var formattedprojectTitle = HTMLprojectTitle.replace("%data%", project.title);
+//var formattedprojectDates = HTMLprojectDates.replace("%data%", project.dates);
+//var formattedprojectDescription = HTMLprojectDescription.replace("%data%", project.description);
+//var formattedprojectImage = HTMLprojectImage.replace("%data%", project.image);
+//
+//$("#projects")
+//	.append(HTMLprojectStart)
+//	.append(formattedprojectTitle)
+//	.append(formattedprojectDates)
+//	.append(formattedprojectDescription)
+//	.append(formattedprojectImage);
+//
+//var formattedName = HTMLschoolName.replace("%data%", education.schools[0].name);
+//var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[0].degree);
+//var formattedDate = HTMLschoolDates.replace("%data%", education.schools[0].date);
+//var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[0].location);
+//var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[0].major);
+//
+//$("#education")
+//	.append(HTMLschoolStart)
+//	.append(formattedName)
+//	.append(formattedDegree)
+//	.append(formattedDate)
+//	.append(formattedLocation)
+//	.append(formattedMajor);
+//
+//var formattedonlineTitle = HTMLonlineTitle.replace("%data%", online.title);
+//var formattedonlineSchool = HTMLonlineSchool.replace("%data%", online.school);
+//var formattedonlineDate = HTMLonlineDates.replace("%data%", online.date);
+//var formattedonlineURL = HTMLonlineURL.replace("%data%", online.URL);
+//
+//$("#education")
+//	.append(HTMLonlineClasses)
+//	.append(formattedonlineTitle)
+//	.append(formattedonlineDate)
+//	.append(formattedonlineURL);
